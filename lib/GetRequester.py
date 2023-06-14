138c2d4f852cc1bfd42691f4d3406ebd62f75014
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-06-14 16:08:10 -0400

    passed testing

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,18 @@ import requests
 import json
 
 class GetRequester:
+    
+    dbUrl = ''
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        dbUrl = self.url
+        response = requests.get(dbUrl)
+        return response.content
 
     def load_json(self):
-        pass
+        json_response = json.loads(self.get_response_body())
+        return json_response
+        
